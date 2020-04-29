1b5d44351962815a8909f960df01f88b19f08394
Martin Araya <martin.araya.zavala@gmail.com>
2020-04-28 22:48:03 -0400

    Created mole Reporte

--- sosafitosapp/models.py
+++ sosafitosapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+class Reporte(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    foto = models.ImageField(upload_to='Reportes')
+    fecha = models.DateField(default=timezone.now)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
