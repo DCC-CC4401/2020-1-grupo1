77c2496c5fb5002290aa125f4693f6ebf8a59853
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-05-31 21:49:45 -0400

    updated models.py with new Tag model

--- sosafitosapp/models.py
+++ sosafitosapp/models.py
@@ -4,9 +4,23 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+
 class Reporte(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
-    foto = models.ImageField(upload_to='Reportes')
+    foto = models.ImageField(upload_to="Reportes")
     fecha = models.DateField(default=timezone.now)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Tag(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    reportes = models.ManyToManyField(Reporte, related_name="tags")
+
+    def __str__(self):
+        return self.nombre
