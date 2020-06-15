b392a1c0bc248e6e3cf8321d18c4c58b44df2de9
Martin <martin.araya.zavala@gmail.com>
2020-06-15 19:10:58 -0400

    Agregadas ubicaciones a reportes

--- sosafitosapp/models.py
+++ sosafitosapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from location_field.models.plain import PlainLocationField
 from PIL import Image
 
 # Create your models here.
@@ -11,6 +12,8 @@ class Reporte(models.Model):
     foto = models.ImageField(upload_to='Reportes')
     fecha = models.DateField(default=timezone.now)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    ciudad = models.CharField(max_length=255)
+    ubicacion = PlainLocationField(based_fields=['ciudad'], zoom=7)
 
     def __str__(self):
         return self.titulo
